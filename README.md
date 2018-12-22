4e0339a57bfa2774e841d14461033b6e0946c433
hii600 <ikeuchi@rainbow.iis.u-tokyo.ac.jp>
2018-12-22 16:35:30 +0900

    updated README.md and .gitignore

---
+++ README.md
@@ -0,0 +1,18 @@
+# FX sender scraping from Yahoo web page (in Japanese)
+1.
+プログラムをコピーし編集
+```
+cd FX-sender/src
+cp cron.conf.sample cron.conf
+cp run-get_fx_rates.sh.sample run-get_fx_rates.sh
+```
+デフォルトでは日曜以外の毎日17時にメール送信するよう設定
+cron.confで希望配信日時および実行ディレクトリの絶対パスを指定
+run-get_fx_rates.shでは送受信メールアドレスを指定
+
+2.
+cronを実行
+```
+crontab cron.conf
+```
+
--- src/get_fx_rates.py
+++ src/get_fx_rates.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import os 
+import os
 import sys
 import numpy as np
 import urllib2
@@ -9,8 +9,6 @@ import datetime
 
 """
 Get FX rates by scraping
-On 24 Aug, 2018
-By Hiroaki Ikeuchi
 """
 
 def print_source(url):
@@ -26,12 +24,14 @@ def get_css_text(arg):
 def calc_change(fx, ba_flt, ba_str):
     if bool_yestday:
         return " (%+.3f)" % (ba_flt - dict_yestday[fx+ba_str])
+    else:
+        return None
 
 def print_fx(fx):
     print get_css_text(".ico{}Jpy{}".format(fx.capitalize(), yr)).encode("utf-8")
     bid = float(get_css_text("#{}JPY_chart_bid".format(fx)))
     ask = float(get_css_text("#{}JPY_chart_ask".format(fx)))
-    print "bid: %.3f" % bid, calc_change(fx, bid, "bid") 
+    print "bid: %.3f" % bid, calc_change(fx, bid, "bid")
     print "ask: %.3f" % ask, calc_change(fx, ask, "ask")
     print
 
@@ -45,32 +45,33 @@ def get_dict_yestday():
     return dict_yestday
 
 def get_ctime():
-    ctime = datetime.datetime.now() 
+    ctime = datetime.datetime.now()
     print ctime.strftime("%Y/%m/%d %H:%M時点"), u"(前日比)".encode("utf-8")
-    print 
+    print
     return ctime
 
-# Current time
-ctime = get_ctime()
-yr = ctime.strftime("%y")
-# FX list
-fx_list = "GBP EUR USD AUD".split()
-# read yesterday's rates
-fn_yestday = "../data/{}.txt".format((ctime - datetime.timedelta(days=1)).strftime("%Y%m%d"))
-bool_yestday = os.path.exists(fn_yestday)
-dict_yestday = get_dict_yestday()
-# output list
-fn_out = "../data/{}.txt".format(ctime.strftime("%Y%m%d"))
-list_out = []
-# BeautifulSoup
-url = "https://info.finance.yahoo.co.jp/fx/list/"
-instance = urllib2.urlopen(url)
-soup = BeautifulSoup(instance, "html.parser")
-# main
-for fx in fx_list:
-    print_fx(fx)
-    add_fxValues(fx)
-# print source
-print_source(url)
-# save output
-np.savetxt(fn_out, list_out, fmt="%e")
+if __name__=='__main__':
+    # current time
+    ctime = get_ctime()
+    yr = ctime.strftime("%y")
+    # FX list
+    fx_list = "GBP EUR USD AUD".split()
+    # read yesterday's rates
+    fn_yestday = "../data/{}.txt".format((ctime - datetime.timedelta(days=1)).strftime("%Y%m%d"))
+    bool_yestday = os.path.exists(fn_yestday)
+    dict_yestday = get_dict_yestday()
+    # output list
+    fn_out = "../data/{}.txt".format(ctime.strftime("%Y%m%d"))
+    list_out = []
+    # BeautifulSoup
+    url = "https://info.finance.yahoo.co.jp/fx/list/"
+    instance = urllib2.urlopen(url)
+    soup = BeautifulSoup(instance, "html.parser")
+    # main
+    for fx in fx_list:
+        print_fx(fx)
+        add_fxValues(fx)
+    # print source
+    print_source(url)
+    # save output
+    np.savetxt(fn_out, list_out, fmt="%e")
